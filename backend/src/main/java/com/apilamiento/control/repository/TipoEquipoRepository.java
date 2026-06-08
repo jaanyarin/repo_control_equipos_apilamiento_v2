514ed72bb91b2a7dff8c5b617b63ef98194b3b4c
Alejandro_Anyarin <alejandro.anyarin@gmail.com>
2026-06-08 12:49:47 -0500

    fix(backend): completar TipoEquipoRepository

--- backend/src/main/java/com/apilamiento/control/repository/TipoEquipoRepository.java
+++ backend/src/main/java/com/apilamiento/control/repository/TipoEquipoRepository.java
@@ -4,10 +4,8 @@ import com.apilamiento.control.entity.TipoEquipo;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.List;
 import java.util.Optional;
 
 @ApplicationScoped
-public class TipoEquipoRepository implements PanacheRepository<TipoEquipo> {
-
-    public Optional<TipoEquipo> findByCodigo(String codigo) {
-        return find("codigo", codigo).firstResultOptional();
+public class TipoEquipoRepository implements PanacheRepository
